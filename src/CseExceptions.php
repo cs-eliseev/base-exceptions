e030cc798ba523dc6d3c4ce7f56a414bbadb34bb
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-01 08:18:34 +0300

    add method get error msg

--- src/CseExceptions.php
+++ src/CseExceptions.php
@@ -19,4 +19,16 @@ class CseExceptions extends Exception
      * @var array
      */
     private static $errorsMsg = [];
+
+    /**
+     * Get error msg
+     *
+     * @param int $code
+     *
+     * @return string
+     */
+    public static function getErrorMsg(int $code): ?string
+    {
+        return static::$errorsMsg[$code] ?? self::DEFAULT_ERROR_MSG;
+    }
 }
