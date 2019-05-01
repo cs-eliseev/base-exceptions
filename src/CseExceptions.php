0105f3d934c5a48a260a3d6119fdd1e3e33b3e73
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-01 08:16:18 +0300

    create exception object

---
+++ src/CseExceptions.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace cse\base;
+
+use Exception;
+
+/**
+ * Class CseException
+ *
+ * @package cse\based\extend
+ */
+class CseExceptions extends Exception
+{
+
+}
